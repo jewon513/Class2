930f3c397dd216fdc37fa51f7e1d6878312222fb
jewon513 <jewon513@gmail.com>
2019-10-08 16:36:16 +0900

    2019-10-08

--- Java_34_SingleTone/src/com/biz/single/service/SafeLazySingleV2.java
+++ Java_34_SingleTone/src/com/biz/single/service/SafeLazySingleV2.java
@@ -4,7 +4,9 @@ public class SafeLazySingleV2 {
 
 	private SafeLazySingleV2() {}
 	
-	public static SafeLazySingleV2 instance;
+	public volatile static SafeLazySingleV2 instance;
+	// 잘 관리하지 않으면 날아간다.
+	// 컴퓨터 하드웨어적인 Main memory 영역에 저장해서 운영체제가 관리하도록 하라.
 	
 	public static SafeLazySingleV2 getInstance() {
 		
